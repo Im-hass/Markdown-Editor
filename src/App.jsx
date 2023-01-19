0adce146dd0a2f6f21392d88f7c657bf8b836855
Im-Hass <huiseon4107@gmail.com>
2023-01-19 23:56:47 +0900

    Fix: 필요없는 파일 삭제, 확장자 변경 App.js -> App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import MarkdownEditor from "./MarkdownEditor";
+import "./main.css";
+
+function App() {
+    return (
+        <div className="App">
+            <MarkdownEditor />
+        </div>
+    );
+}
+
+export default App;
