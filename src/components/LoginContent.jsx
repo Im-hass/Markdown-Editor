3c1d4a83415ec1d1b3ffb3e7f421da4d168ba4b0
Im-Hass <huiseon4107@gmail.com>
2023-02-12 03:12:55 +0900

    Refactor: Login Page, Content 분리

---
+++ src/components/LoginContent.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { login } from "../store/api/login";
+import { useNavigate } from "react-router-dom";
+
+function LoginContent() {
+    const navigate = useNavigate();
+    const [inputData, setInputData] = useState({
+        loginId: "",
+        password: "",
+    });
+
+    const handleSubmitLogin = (e) => {
+        e.preventDefault();
+
+        if (inputData.loginId.length !== 0 && inputData.password.length !== 0) {
+            login(inputData).then((res) => {
+                if (res.data.data.result) {
+                    localStorage.setItem("token", res.headers.authorization);
+                    navigate("/list");
+                } else {
+                    console.log("로그인 실패");
+                    console.log(res);
+                }
+            });
+        }
+    };
+
+    const handleInputValue = (e) => {
+        console.log(e.target.name);
+        console.log(e.target.value);
+        setInputData({
+            ...inputData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    return (
+        <div>
+            <h1>로그인</h1>
+            <form onSubmit={handleSubmitLogin}>
+                <div className="box">
+                    <span>Id</span>
+                    <input
+                        type="text"
+                        name="loginId"
+                        id="loginId"
+                        onChange={handleInputValue}
+                    />
+                </div>
+                <div className="box">
+                    <span>Password</span>
+                    <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        onChange={handleInputValue}
+                    />
+                </div>
+                <button type="submit" className="btn">
+                    로그인
+                </button>
+            </form>
+        </div>
+    );
+}
+
+export default LoginContent;
